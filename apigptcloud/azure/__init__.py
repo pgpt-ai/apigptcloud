bd387ea9598e444521d1abd90464295159b549eb
gantianyuneil <gantianyuneil@foxmail.com>
2023-08-04 14:46:25 +0800

    feat: add Azure Completion

---
+++ apigptcloud/azure/__init__.py
@@ -0,0 +1,11 @@
+from . import (
+    ChatCompletion,
+    Completion,
+    # Embedding,
+)
+
+
+resource_name: str = ""
+deployment_name: str = ""
+api_version: str = ""
+api_key: str = ""
--- apigptcloud/openai/Completion.py
+++ apigptcloud/openai/Completion.py
@@ -2,8 +2,6 @@ import requests
 from apigptcloud import openai
 import json
 
-valid_args = ["temperature", "stream", "max_tokens"]
-
 
 def create(model: str, prompt: str, **kwargs):
     url = openai.api_base + "/completions"
@@ -17,17 +15,6 @@ def create(model: str, prompt: str, **kwargs):
     }
     # validate kwargs
     for arg in kwargs:
-        if arg not in valid_args:
-            raise ValueError(f"Invalid argument: {arg}")
-        else:
-            if arg == "temperature":
-                if isinstance(kwargs[arg], float):
-                    data["temperature"] = kwargs[arg]
-            elif arg == "stream":
-                if isinstance(kwargs[arg], bool):
-                    data["stream"] = kwargs[arg]
-            elif arg == "max_tokens":
-                if isinstance(kwargs[arg], int):
-                    data["max_tokens"] = kwargs[arg]
+        data[arg] = kwargs[arg]
 
     return json.dumps(requests.post(url, headers=headers, json=data).json(), indent=2)
