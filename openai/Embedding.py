7c5083b93395f3455efd92cd355b98d3c4cfb299
gantianyuneil <gantianyuneil@foxmail.com>
2023-08-04 11:06:32 +0800

    feat: add Embedding

---
+++ openai/Embedding.py
@@ -0,0 +1,17 @@
+import requests
+import openai
+import json
+
+
+def create(model: str, input):
+    url = openai.api_base + "/embeddings"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer " + openai.api_key
+    }
+    data = {
+        "model": model,
+        "input": input,
+    }
+    return json.dumps(requests.post(url, headers=headers, json=data).json(), indent=2)
+
