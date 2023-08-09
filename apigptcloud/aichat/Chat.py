110d202b41130066c0bc1789a7d5f398d299b8a5
gantianyuneil <gantianyuneil@foxmail.com>
2023-08-09 16:33:08 +0800

    refactor: Chat urls

--- apigptcloud/aichat/Chat.py
+++ apigptcloud/aichat/Chat.py
@@ -3,15 +3,19 @@ from apigptcloud import aichat
 import json
 
 
-def openai(query: str, integration_id: int):
-    url = aichat.url + "/api/pgcompletion/document/chat/"
-    headers = aichat.global_headers
-    payload = {'integration_id': integration_id, 'query': query, 'llm': 'openai'}
-    return json.dumps(requests.request("POST", url, headers=headers, data=payload), indent=2)
+def openai(query: str):
+    url = aichat.api_base + "/v1/chat/completions/"
+    headers = {
+        'Authorization': aichat.api_key
+    }
+    payload = {'query': query, 'llm': 'openai'}
+    return requests.request("POST", url, headers=headers, data=payload).json()
 
 
-def chatglm(query: str, integration_id: int):
-    url = aichat.url + "/api/pgcompletion/document/chat/"
-    headers = aichat.global_headers
-    payload = {'integration_id': integration_id, 'query': query, 'llm': 'chatglm'}
-    return json.dumps(requests.request("POST", url, headers=headers, data=payload), indent=2)
+def chatglm(query: str):
+    url = aichat.api_base + "/v1/chat/completions/"
+    headers = {
+        'Authorization': aichat.api_key
+    }
+    payload = {'query': query, 'llm': 'chatglm'}
+    return requests.request("POST", url, headers=headers, data=payload).json()
