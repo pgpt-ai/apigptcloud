495baf88f16371a0a93f3c450d555e9357d6a457
gantianyuneil <gantianyuneil@foxmail.com>
2023-08-10 12:41:37 +0800

    feat: add embedding-excel

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'All in one AIGC API package for Python'
 LONG_DESCRIPTION = 'All in one AIGC API package for Python'
 
