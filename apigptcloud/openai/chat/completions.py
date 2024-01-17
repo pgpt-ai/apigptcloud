82beeededdcde8990ed6bbef36c3243ed963c398
gantianyuneil <gantianyuneil@foxmail.com>
2024-01-17 05:14:00 -0500

    fix: stream

--- apigptcloud/openai/chat/completions.py
+++ apigptcloud/openai/chat/completions.py
@@ -21,10 +21,15 @@ def create(model: str, messages: list, **kwargs):
         data[arg] = kwargs[arg]
 
     if model not in supported_models:
-        return {'detail': [{'loc': ['body', 'model'], 'msg': "value is not a valid enumeration member; permitted: 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k', 'gpt-4-turbo', 'gpt-3.5-turbo-instruct'", 'type': 'type_error.enum', 'ctx': {'enum_values': ['gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k', 'gpt-4-turbo', 'gpt-3.5-turbo-instruct']}}]}
+        return {'detail': [{'loc': ['body', 'model'],
+                            'msg': "value is not a valid enumeration member; permitted: 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k', 'gpt-4-turbo', 'gpt-3.5-turbo-instruct'",
+                            'type': 'type_error.enum', 'ctx': {
+                'enum_values': ['gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k', 'gpt-4-turbo',
+                                'gpt-3.5-turbo-instruct']}}]}
 
-    if 'stream' in kwargs:
-        if kwargs['stream']:
+    if 'stream' in kwargs and kwargs['stream']:
+        print("stream")
+        def process():
             response = requests.post(url, headers=headers, json=data, stream=True).iter_lines()
             for chunk in response:
                 if chunk:
@@ -34,6 +39,8 @@ def create(model: str, messages: list, **kwargs):
                     answer = json.loads(parse)
                     if len(answer) > 0:
                         yield answer
-            return response
-        else:
-            return requests.post(url, headers=headers, json=data).json()
+        return process()
+        # return response
+    else:
+        print("else")
+        return requests.post(url, headers=headers, json=data).json()
