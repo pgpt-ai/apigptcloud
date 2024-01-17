bc54323cebfc89920ba9c204a3a9f859bddf4439
gantianyuneil <gantianyuneil@foxmail.com>
2024-01-17 05:16:50 -0500

    fix: stream

--- apigptcloud/openai/chat/completions.py
+++ apigptcloud/openai/chat/completions.py
@@ -21,26 +21,22 @@ def create(model: str, messages: list, **kwargs):
         data[arg] = kwargs[arg]
 
     if model not in supported_models:
-        return {'detail': [{'loc': ['body', 'model'],
-                            'msg': "value is not a valid enumeration member; permitted: 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k', 'gpt-4-turbo', 'gpt-3.5-turbo-instruct'",
-                            'type': 'type_error.enum', 'ctx': {
-                'enum_values': ['gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k', 'gpt-4-turbo',
-                                'gpt-3.5-turbo-instruct']}}]}
-
-    if 'stream' in kwargs and kwargs['stream']:
-        print("stream")
-        def process():
-            response = requests.post(url, headers=headers, json=data, stream=True).iter_lines()
-            for chunk in response:
-                if chunk:
-                    parse = chunk.decode('utf-8').split('data: ')[-1]
-                    if parse == "[DONE]":
-                        break
-                    answer = json.loads(parse)
-                    if len(answer) > 0:
-                        yield answer
-        return process()
-        # return response
+        return {'detail': [{'loc': ['body', 'model'], 'msg': "value is not a valid enumeration member; permitted: 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k', 'gpt-4-turbo', 'gpt-3.5-turbo-instruct'", 'type': 'type_error.enum', 'ctx': {'enum_values': ['gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-32k', 'gpt-4-turbo', 'gpt-3.5-turbo-instruct']}}]}
     else:
-        print("else")
-        return requests.post(url, headers=headers, json=data).json()
+        if 'stream' in kwargs and kwargs['stream']:
+            # print("stream")
+            def process():
+                response = requests.post(url, headers=headers, json=data, stream=True).iter_lines()
+                for chunk in response:
+                    if chunk:
+                        parse = chunk.decode('utf-8').split('data: ')[-1]
+                        if parse == "[DONE]":
+                            break
+                        answer = json.loads(parse)
+                        if len(answer) > 0:
+                            yield answer
+            return process()
+            # return response
+        else:
+            # print("else")
+            return requests.post(url, headers=headers, json=data).json()
