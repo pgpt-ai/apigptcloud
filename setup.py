d763078f8605ee1392182e94150723ef1ac7c91e
Randa168 <155437101+Randa168@users.noreply.github.com>
2025-04-15 14:26:57 +0800

    refactor: long description

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup, find_packages
 
 VERSION = '0.1.0'
 DESCRIPTION = 'All in one AIGC API package for Python'
-LONG_DESCRIPTION = 'All in one AIGC API package for Python'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="pgptAI",
@@ -10,7 +12,8 @@ setup(
     author="gantianyuneil",
     author_email="apigpt@quseit.com",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=['requests', 'typing_extensions'],
     keywords=['python', 'first package'],
